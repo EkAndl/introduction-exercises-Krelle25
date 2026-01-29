3dff897edf60da681dc958dd96e6126fa2f32755
Kristian Meyer Steffensen <krst0003@stud.ek.dk>
2026-01-29 11:47:25 +0100

    Basics exercises in JavaScript

--- 1._Basics.js
+++ 1._Basics.js
@@ -10,6 +10,10 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log("My first name is " + firstName + " and my last name is " + lastName)
+
+// backticks `, String template literal = whole line, String interpolation = ${}
+console.log(`My first name is ${firstName} and my last name is ${lastName}`)
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -20,6 +24,8 @@ const increment = 1;
 // Add the year plus the increment
 // The result should be 2026
 // You cannot touch the first or the second line
+
+console.log(parseInt(year) + increment)
 
 
 // --------------------------------------
